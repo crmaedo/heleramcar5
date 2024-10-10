4bd56f968ea254595ddf20bc9340d2c01729f496
Ramón Amézquita <rameviz@icloud.com>
2024-10-10 13:25:45 +0200

    Introducing :class:`Endpoint`

--- R/api.R
+++ R/api.R
@@ -1,7 +1,47 @@
 library(httr2)
 
+#' Endpoint
+#'
+#' Executes http requests against the specified URL.
+#'
+#' @field base_url character. Base URL.
+Endpoint <- setRefClass(
+  "Endpoint",
+  fields = list(base_url = "character")
+)
 
-make_api_call <- function(url) url |> request() |> req_perform()
+
+Endpoint$methods(
+  make_api_call = function(url = "") {
+    url <- paste0(base_url, url)
+    url |> request() |> req_perform()
+  }
+)
+
+
+# Creates instances for :class:`Endpoint`.
+# These will be used throughout the project for making api calls.
+docEndpoint <- Endpoint(base_url = "https://data.riksdagen.se/dokument/")
+queryEndpoint <- Endpoint(base_url = "http://data.riksdagen.se/dokumentlista/")
+
+
+
+#' Auxiliary function for counting the number of words in a string.
+#'
+#' @param text Input text.
+#'
+#' @return data.frame
+wordsFrequency <- function(text) {
+
+
+  wordsList <- strsplit(text, split = " ")
+  freqDF <- as.data.frame(table(unlist(wordsList)))
+
+  names(freqDF) <- c("number","times")
+  freqDF$rank <- rank(-freqDF$times, ties.method = "min")
+  freqDF <- freqDF[order(freqDF$rank, decreasing = F),]
+  return(freqDF)
+}
 
 
 #' Searches documents in the Riksdagen database
@@ -11,10 +51,8 @@ make_api_call <- function(url) url |> request() |> req_perform()
 #' @return data.frame
 #' @export
 #' @examples
-#' result <- searchdocs(query = "budget")
-searchdocs <- function(query) {
-
-  base_url <- "http://data.riksdagen.se/dokumentlista/"
+#' result <- queryDocs(query = "budget")
+queryDocs <- function(query) {
 
   include <- c(
     "traff", "domain", "database", "datum", "id", "rdrest", "slutdatum",
@@ -23,26 +61,23 @@ searchdocs <- function(query) {
     "titel", "dokumentnamn", "score", "url"
     )
 
-  cleandoc <- function(document) {
+
+  cleanDoc <- function(document) {
     document <- document[include]
     nullmask <- as.vector(sapply(document, is.null))
     document[nullmask] <- NA
     return(document)
   }
 
-  url <- paste0(
-    base_url,
-    "?sok=",
-    query,
-    "&utformat=json&sort=datum&sortorder=desc"
-    )
+  # Construct URL and make api call.
+  url <- paste0("?sok=", query, "&utformat=json&sort=datum&sortorder=desc")
+  jsonResp <- url |> queryEndpoint$make_api_call() |> resp_body_json()
 
   # Extract list of documents from json response.
-  jsonresp <- url |> make_api_call() |> resp_body_json()
-  docs <- jsonresp$dokumentlista$dokument
+  docs <- jsonResp$dokumentlista$dokument
 
   # Return data.frame
-  t(data.frame(sapply(docs, cleandoc)))
+  t(data.frame(sapply(docs, cleanDoc)))
 }
 
 
@@ -54,18 +89,16 @@ searchdocs <- function(query) {
 #' @export
 #' @references https://www.riksdagen.se/sv/dokument-och-lagar/riksdagens-oppna-data/anvandarstod/sa-fungerar-dokument-id/
 #' @examples
-#' document <- get_document("GZ01MJU21")
-getdocument <- function (document_id) {
-  base_url <- "https://data.riksdagen.se/dokument/"
-  url <- paste0(base_url, document_id)
-  make_api_call(url)
+#' doc <- getDoc(document_id = "sfs-2016-1091")
+getDoc <- function (document_id) {
+
+  resp <- docEndpoint$make_api_call(url = document_id)
+
+  extract_text <- function(resp) {
+    xmldata <- resp_body_xml(resp)
+    xmllist <- as_list(xmldata)
+    return(xmllist$dokumentstatus$dokument$text[[1]])
+  }
+
+  return (resp |> extract_text())
 }
-
-
-
-
-
-
-
-
-
